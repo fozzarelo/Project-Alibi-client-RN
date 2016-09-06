976e2629389515188810199b26ac7c2d23bc311f
Dan Pinto <fozzarelo@gmail.com>
2016-09-05 17:22:59 -0700

    Testing

---
+++ src/components/helpers/constants-example.js
@@ -0,0 +1,15 @@
+
+// Rename to constants.js
+
+appData = {
+  // Heroku Server
+  'urlBase': 'https://alibi-serv.herokuapp.com/api/v1',
+  // Local Server
+  // 'urlBase': 'http://localhost:3000/api/v1',
+  'urlToken': 'tok109tok',
+
+  'accessKey': 'Add your AWS keys here',
+  'secretKey': 'Add your AWS keys here',
+}
+
+export default appData
